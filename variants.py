25bfa4303a2b004156b5662005090cd80ef919cf
Toomore Chiang <toomore0929@gmail.com>
2014-02-02 11:53:40 +0800

    Add time mark.

--- variants.py
+++ variants.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 import csv
+from datetime import datetime
 
 
 with open('./list.csv') as files:
     csv_files = csv.reader(files)
     with open('./list-1.csv', 'w') as files_w:
-        print 'start list-1'
+        print datetime.now(), 'start list-1'
         csv_files_w = csv.writer(files_w)
 
         # Remove png words.
         for words in csv_files:
             if 'png' not in words[-1]:
                 csv_files_w.writerow(words)
-        print 'ending list-1'
+        print datetime.now(), 'ending list-1'
 
     with open('./list-2.csv', 'w') as files_w_2:
-        print 'start list-2'
+        print datetime.now(), 'start list-2'
         csv_w = csv.writer(files_w_2)
         with open('./list-1.csv') as files_r:
-            csv_r = [i for i in csv.reader(files_r)]
+            csv_r = list(csv.reader(files_r))
             loops = 0
             for words in csv_r:
                 loops += 1
@@ -29,4 +30,4 @@ with open('./list.csv') as files:
                                 get_words[-2] is not target[-2]:
                             csv_w.writerow((get_words[-2], target[-2]))
                             continue
-        print 'ending list-2'
+        print datetime.now(), 'ending list-2'
